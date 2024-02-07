419969b7bf8023ad888e961b73afdd9af1d58b5d
unknown <namitlodh>
2024-02-07 15:17:16 +0530

    [Namit] Add: Delegates

--- Program.cs
+++ Program.cs
@@ -6,36 +6,24 @@ using System.Threading.Tasks;
 
 namespace LambdaExpressions
 {
-    class Student
-    {
-        public int Rollno { get; set; }
-        public string Name { get; set; }
-    }
+    public delegate void AddDelegate(int x, int y);
+    
     internal class Program
     {
-        static void Main()
+        public void Addnums(int a, int b)
         {
-            List<Student> list = new List<Student>()
-            {
-                new Student { Rollno=1, Name="Rahul"},
-                new Student { Rollno=2, Name="Anu"},
-                new Student { Rollno=3, Name="Namit"},
-                new Student { Rollno=4, Name="Dinesh"},
-                new Student { Rollno=5, Name="Siddhant"}
-            };
-            Console.Write("The list of student : \n");
-            foreach (var i in list)
-            {
-                Console.WriteLine($"{i.Rollno} : {i.Name}");
-            }
-            Console.WriteLine();
-
-            var integers=list.OrderBy(x =>x.Name);
-            Console.Write("The updated list is: \n");
-            foreach (var d in integers)
-            {
-                Console.WriteLine($"{d.Rollno} : {d.Name}");
-            }
+            Console.WriteLine(a+b);
+        }
+        public void Mulnums(int a, int b)
+        {
+            Console.WriteLine(a*b);
+        }
+        static void Main(string[] args)
+        { 
+            Program program = new Program();
+            AddDelegate ad = new AddDelegate(program.Addnums);
+            ad += program.Mulnums;
+            ad.Invoke(100, 50);
             Console.ReadLine();
         }
     }
